5e52d40f3a9e218773b1fbce9b3dc65dffd7244c
RomkaKamashev <romka.kamashev@gmail.com>
2024-03-19 11:00:47 +0100

    Modification3
    s28349 instead

--- obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17eda45f36c934e238c17e61641eb2cc2d7d75ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdca0e2734a723cc1f8feb1c9224bf337ecb43f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
